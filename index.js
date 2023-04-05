277b6637a6ebca6703c622d04b2d0281b9cbfb01
Daniel Rivera <dani_wooten@hotmail.com>
2023-04-05 14:51:32 -0700

    First commit

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const multer = require('multer');
 
-var app = express();
+const app = express();
+const upload = multer({ dest: 'uploads/' });
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+app.use(cors());
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).send('No se ha cargado ningún archivo');
+  }
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+const listener = app.listen(process.env.PORT || 3000, function() {
+  console.log('Tu aplicación está escuchando en el puerto ' + listener.address().port);
 });
